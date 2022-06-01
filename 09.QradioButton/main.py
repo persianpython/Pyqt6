aa37f9d8bf414f69ef073646dbc6869bea687fdc
Persian Python <persianpython2000@gmail.com>
2022-06-01 14:11:04 +0430

    Upload Episode 9

---
+++ 09.QradioButton/main.py
@@ -0,0 +1,60 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QHBoxLayout, QRadioButton, QVBoxLayout, QLabel
+from PyQt6.QtGui import QIcon, QFont
+from PyQt6.QtCore import QSize
+import sys
+
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('PersianPython')
+        self.setWindowOpacity(1)
+        self.setWindowIcon(QIcon('logo.png'))
+        self.create_widgets()
+
+    def create_widgets(self):
+        hbox = QHBoxLayout()
+        rad1 = QRadioButton('Python')
+        rad1.setIcon(QIcon('images/python.png'))
+        rad1.setIconSize(QSize(40,40))
+        rad1.setFont(QFont('Arial', 15))
+        rad1.setChecked(True)
+        rad1.toggled.connect(self.radio_selected)
+
+        hbox.addWidget(rad1)
+
+        rad2 = QRadioButton('Java')
+        rad2.setIcon(QIcon('images/java.png'))
+        rad2.setIconSize(QSize(40,40))
+        rad2.setFont(QFont('Arial', 15))
+        rad2.toggled.connect(self.radio_selected)
+
+        hbox.addWidget(rad2)
+
+        rad3 = QRadioButton('JavaScript')
+        rad3.setIcon(QIcon('images/javascript.png'))
+        rad3.setIconSize(QSize(40,40))
+        rad3.setFont(QFont('Arial', 15))
+        rad3.toggled.connect(self.radio_selected)
+
+        hbox.addWidget(rad3)
+
+        vbox = QVBoxLayout()
+        self.lable = QLabel()
+        vbox.addWidget(self.lable)
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+
+    def radio_selected(self):
+        radio_btn = self.sender()
+        if radio_btn.isChecked():
+            print(radio_btn.text())
+            self.lable.setText(radio_btn.text())
+
+
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+sys.exit(app.exec())
