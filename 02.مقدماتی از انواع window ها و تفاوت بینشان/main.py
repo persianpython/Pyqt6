28b2552234359022291138cf23676cf74d1b8bc9
Persian Python <persianpython2000@gmail.com>
2022-06-01 13:34:37 +0430

    Upload Episode 2

---
+++ 02.مقدماتی از انواع window ها و تفاوت بینشان/main.py
@@ -0,0 +1,25 @@
+# pip install pyqt6
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QDialog
+import sys
+from PyQt6.QtGui import QIcon
+
+app = QApplication(sys.argv)
+
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        # self.menuBar().addMenu('File')
+        # self.statusBar().showMessage("Salam")
+        # self.setMaximumSize(500, 700)
+        # self.setMinimumSize(100, 100)
+        self.setFixedWidth(500)
+        self.setWindowTitle('PersianPython')
+        self.setWindowOpacity(1)
+        self.setWindowIcon(QIcon('logo.png'))
+
+window = MyWindow()
+
+window.show()
+
+sys.exit(app.exec())
